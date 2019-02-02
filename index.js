38c2028912b0bac9853928b99b6834d0b231d7cf
jgaull <Jonbeesh@gmail.com>
2019-02-02 13:47:19 -0800

    Revert "Roku events"

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 
+const { Client, keys } = require('roku-client')
 const Etekcity = require('etekcity-smartplug')
-const RokuTV = require('./roku-tv')
+const delay = require('delay')
 
 //setup the switch
 const config = require('./env.json')
@@ -19,26 +20,81 @@ let device = etekcity.login(config.username, config.password).then(() => {
     return device
 })
 
-const tv = new RokuTV(config.tvIp)
+let wasOn
+function poll(client, callback) {
 
-tv.on('power-on', () => {
-    Promise.resolve(device).then(device => {
+    client.info().then(info => {
 
-        console.log(`turning device ${device.name} on`)
-        return etekcity.turnOn(device.id)
+        const isOn = TVIsOn(info)
+
+        if (isOn != wasOn) {
+            wasOn = isOn
+            callback(isOn)
+        }
+
+        const milliseconds = isOn ? 5000 : 1000
+        return delay(milliseconds)
+
+    }).then(() => {
+        poll(client, callback)
 
     }).catch(error => {
-        console.log(`error turning outlet ${isOn ? 'on' : 'off'}: ${error.stack}`)
+        console.log(error.stack)
+        poll(client, callback)
+    })
+}
+
+function TVIsOn(info) {
+    return info.powerMode === 'PowerOn'
+}
+
+const client = new Client(config.tvIp)
+poll(client, tvIsOn => {
+
+    Promise.resolve(device).then(device => {
+        
+        const isOn = outletIsOn(device)
+        if (!tvIsOn) {
+            console.log(`turning device ${device.name} off`)
+            return etekcity.turnOff(device.id)
+        }
+        else if (tvIsOn) {
+            console.log(`turning device ${device.name} on`)
+            return etekcity.turnOn(device.id)
+        }
+    }).catch(error => {
+        console.log('error turning outlet', isOn ? 'on:' : 'off:', error.stack)
     })
 })
 
-tv.on('power-off', () => {
-    Promise.resolve(device).then(device => {
+function outletIsOn(device) {
+    return device.status === 'open'
+}
 
-        console.log(`turning device ${device.name} off`)
-        return etekcity.turnOff(device.id)
-
-    }).catch(error => {
-        console.log(`error turning outlet ${isOn ? 'on' : 'off'}: ${error.stack}`)
+/*
+let client = Client.discover()
+    .then((client) => {
+        console.log(`roku device found at ${client.ip}`);
+        return client.apps();
     })
+    .then((apps) => {
+        apps.forEach(app => console.log(app));
+        // [{ id, name, type, version }, ...]
+    })
+    .catch(err => {
+        console.error(err.stack)
+    });
+*/
+
+/*
+.then((device) => {
+    if (device.status === 'open') {
+        console.log(`turning device ${device.name} off`);
+        return etekcity.turnOff(device.id);
+    }
+    else {
+        console.log(`turning device ${device.name} on`);
+        return etekcity.turnOn(device.id);
+    }
 })
+*/
