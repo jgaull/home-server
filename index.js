7311cd6f39f9df4607e7f2287217eaafc4d9b82c
Jonathan Gaull <jonbeesh@gmail.com>
2021-04-11 20:13:59 -0700

    breaking stuff

--- index.js
+++ index.js
@@ -2,21 +2,36 @@
 
 const Etekcity = require('etekcity-smartplug')
 const RokuTV = require('./roku-tv')
+const ChromecastAPI = require('chromecast-api')
 const config = require('./env.json')
 
 async function main() {
 
     const etekcity = new Etekcity()
-    await etekcity.login(config.username, config.password)
+    try {
+        await etekcity.login(config.username, config.password)
+    }
+    catch (error) {
+        console.log('error with etekcity login:', error.message)
+        return
+    }
     
-    const devices = await etekcity.getDevices()
+    let devices
+    try {
+        devices = await etekcity.getDevices()
+    }
+    catch (error) {
+        console.log('error with etekcity find devices:', error.message)
+        return
+    }
+    
     const device = devices.find((device) => {
         return device.name.includes(config.deviceName)
     })
 
-    const tv = new RokuTV(config.tvName)
+    const rokuTV = new RokuTV(config.tvName)
 
-    tv.on('power-on', () => {
+    rokuTV.on('power-on', () => {
 
         try {
             console.log(`turning device ${device.name} on`)
@@ -27,7 +42,7 @@ async function main() {
         }
     })
 
-    tv.on('power-off', () => {
+    rokuTV.on('power-off', () => {
 
         try {
             console.log(`turning device ${device.name} off`)
@@ -37,6 +52,27 @@ async function main() {
             console.log(`error turning outlet on: ${error.stack}`)
         }
     })
+
+    console.log('chromecast API')
+    const client = new ChromecastAPI()
+    
+    client.on('device', function (device) {
+    
+        var mediaURL = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4';
+        
+        console.log('device:', JSON.stringify(device))
+
+        /*
+        device.play(mediaURL, function (err) {
+            if (!err) console.log('Playing in your chromecast')
+        })
+        */
+    })
 }
 
-main()
+try {
+    main()
+}
+catch (error) {
+    console.log('top level error:', error.message)
+}
