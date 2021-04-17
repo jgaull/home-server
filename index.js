947d7da4b48b41b071a725c96f66d3ba07442a6b
Jonathan Gaull <jonbeesh@gmail.com>
2021-04-17 09:31:18 -0700

    just fucking around

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 
 const Etekcity = require('etekcity-smartplug')
 const RokuTV = require('./roku-tv')
-const ChromecastAPI = require('chromecast-api')
 const config = require('./env.json')
+var nodecast = require('nodecast');
+const { delay } = require('lodash');
 
 async function main() {
 
@@ -52,16 +53,82 @@ async function main() {
             console.log(`error turning outlet on: ${error.stack}`)
         }
     })
+}
 
-    console.log('chromecast API')
+async function fireplace() {
+
+    console.log('starting fireplace server')
+ 
+    var devices = nodecast.find();
+    
+    devices.on('device', function(device) {
+
+        console.log('device found:', JSON.stringify(device))
+
+        if (device.name == 'Shared Screen') {
+
+            var yt = device.app('YouTube')
+            console.log('is Roku? ',device.is('roku'))
+            
+            console.log('starting YouTube Video')
+            yt.start('v=AWKzr6n0ea0', function(err) {
+                // starts the app on the device
+                // also optionally takes data to pass to the app
+                // (for example: youtube takes v=id to launch with a video)
+                if (err) {
+                    console.log('error playing youtube video: ', err.message)
+                }
+            })
+
+
+        }
+
+        /*
+        var yt = device.app('YouTube');
+    
+        yt.start('v=12345', function(err) {
+            // starts the app on the device
+            // also optionally takes data to pass to the app
+            // (for example: youtube takes v=id to launch with a video)
+        });
+        */
+    });
+}
+
+async function nodecastTest() {
+    var Browser = require('nodecast-js');
+ 
+    var url = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4';
+    var timestamp = 60; // in seconds
+    
+    var browser = new Browser();
+    browser.onDevice(function (device) {
+
+        device.onError(function (err) {
+            console.log(err);
+        });
+        
+        console.log('device:', JSON.stringify(device))
+        console.log('list:', browser.getList()); // list of currently discovered devices
+    
+        //device.play(url, timestamp);
+    });
+    browser.start();
+    
+    setTimeout(function () {
+        browser.destroy(); // destroy your browser
+    }, 20000);
+}
+
+async function chromecastAPITest() {
+    const ChromecastAPI = require('chromecast-api')
+ 
     const client = new ChromecastAPI()
     
     client.on('device', function (device) {
-    
         var mediaURL = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4';
         
         console.log('device:', JSON.stringify(device))
-
         /*
         device.play(mediaURL, function (err) {
             if (!err) console.log('Playing in your chromecast')
@@ -71,7 +138,10 @@ async function main() {
 }
 
 try {
-    main()
+    //main()
+    //fireplace()
+    //nodecastTest()
+    chromecastAPITest()
 }
 catch (error) {
     console.log('top level error:', error.message)
